92006ef12918f6554766f48893de281a2c7aa053
Kasim Siddiqui <kasimsiddiqui@gmail.com>
2015-09-30 10:47:46 -0700

    init

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/notes_dev');
+process.env.APP_SECRET = process.env.APP_SECRET || 'changemechangemechangeme';
 
 app.use(express.static(__dirname + '/build'));
+
 var notesRouter = require(__dirname + '/routes/notes_routes');
+var usersRouter = require(__dirname + '/routes/users_routes');
 app.use('/api', notesRouter);
+app.use('/api', usersRouter);
+
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
