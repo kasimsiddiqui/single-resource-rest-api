fb802f5dc3672632399ed4f1d8d21353c7baad8c
Kasim Siddiqui <kasimsiddiqui@gmail.com>
2015-10-08 09:54:36 -0700

    test

---
+++ test/client/resource_service.js
@@ -0,0 +1,28 @@
+require('../../app/js/client');
+
+describe('resource service', function() {
+  beforeEach(angular.mock.module('notesApp'));
+
+  var ResourceService;
+  var $httpBackend;
+  var notesResource;
+  beforeEach(angular.mock.inject(function(Resource, _$httpBackend_) {
+    ResourceService = Resource;
+    $httpBackend = _$httpBackend_;
+    notesResource = ResourceService('notes');
+  }));
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should make a get requets', function() {
+    $httpBackend.expectGET('/api/notes').respond(200, [{noteBody: 'test note', _id: 1}]);
+    notesResource.getAll(function(err, data) {
+      expect(err).toBe(null);
+      expect(Array.isArray(data)).toBe(true);
+    });
+    $httpBackend.flush();
+  });
+});
